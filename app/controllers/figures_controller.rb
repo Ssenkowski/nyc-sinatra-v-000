bf1acd216f39311bb3f9a69639994a65ebc2d49f
Steven Senkowski <scsenkowski@gmail.com>
2018-01-16 12:34:14 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,25 +1,26 @@
 class FiguresController < ApplicationController
+    
+    get '/figures' do
+      @figures = Figure.all
+      erb :'figures/index'
+    end
 
-  get '/figures' do
-    @figures = Figure.all
-    erb :'/figures/index'
-  end
+    get '/figures/new' do 
+      erb :'figures/new'
+    end
 
-  get '/figures/new' do
-    erb :'/figures/new'
-  end
 
-  get '/figures/:id' do
-    @figure = Figure.find(params[:id])
-    erb :'figures/show'
-  end
+    get '/figures/:id' do
+      @figure = Figure.find(params[:id])
+      erb :'figures/show'
+    end
 
-  get '/figures/:id/edit' do
-    @figure = Figure.find(params[:id])
-    erb :'figures/edit'
-  end
+    get '/figures/:id/edit' do 
+      @figure = Figure.find(params[:id])
+      erb :'figures/edit'
+    end
 
-  post '/figures' do
+    post '/figures' do
       @figure = Figure.create(params["figure"])
       if !params[:landmark][:name].empty?
         @figure.landmarks << Landmark.create(params[:landmark])
@@ -28,15 +29,15 @@ class FiguresController < ApplicationController
       if !params[:title][:name].empty?
         @figure.titles << Title.create(params[:title])
       end
-
+      
       @figure.save
       redirect to "/figures/#{@figure.id}"
     end
 
-    post '/figures/:id' do
+    post '/figures/:id' do 
       @figure = Figure.find(params[:id])
       @figure.update(params[:figure])
-
+      
       if !params[:landmark][:name].empty?
         @figure.landmarks << Landmark.create(params[:landmark])
       end
@@ -44,8 +45,9 @@ class FiguresController < ApplicationController
       if !params[:title][:name].empty?
         @figure.titles << Title.create(params[:title])
       end
-
+      
       @figure.save
       redirect to "/figures/#{@figure.id}"
     end
+
 end
