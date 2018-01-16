a2536f86b00f1ae0e9e10cf5b2cbfeab85983072
Steven Senkowski <scsenkowski@gmail.com>
2018-01-16 12:41:35 +0000

    automatically backed up by learn

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,7 +1,10 @@
-class CreateLandmarks < ActiveRecord::Migration[5.0]
+class CreateLandmarks < ActiveRecord::Migration
   def change
     create_table :landmarks do |t|
       t.string :name
+      t.integer :year_completed
+
+      t.timestamps
     end
   end
 end
